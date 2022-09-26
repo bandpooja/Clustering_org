9844386859da04799bbeeed8741a5a28b5edee87
Bandal <pooja.bandal@ontario.ca>
2022-09-25 22:34:59 -0400

    check

--- unsupervised_clustering/preprocess.py
+++ unsupervised_clustering/preprocess.py
@@ -1,26 +1,48 @@
 import re
 
-
 def remove_abbreviation(org_name: str):
     org_name = org_name.lower()
-    # |(?:\s(a-z)\s)]
-    pattern = r'(?:\.[a-z]\.|\s[a-z]\s|\.\s[a-z]\.)+'
 
-    result = re.findall(pattern, org_name)
-    if result:
-        for r in result:
-            pattern_w = r'(?:[a-z&])'
-            x = re.findall(pattern_w, r)
-            if r[-1] == ' ':
-                org_name = org_name.replace(r, x[0]+ ' ')
+    org_words = org_name.split(' ')
+    org_words_w_aggregated_abb = []
+    for w in org_words:
+        if len(w) == 1:
+            if len(org_words_w_aggregated_abb) == 0:
+                org_words_w_aggregated_abb.append(w)
             else:
-                org_name = org_name.replace(r, x[0])
+                w_l = org_words_w_aggregated_abb.pop()
+                w_l += w
+                org_words_w_aggregated_abb.append(w_l)
+        elif len(w) == 2:
+            if w[-1] == '.':
+                if len(org_words_w_aggregated_abb) == 0:
+                    org_words_w_aggregated_abb.append(w[0])
+                else:
+                    w_l = org_words_w_aggregated_abb.pop()
+                    w_l += w[0]
+                    org_words_w_aggregated_abb.append(w_l)
+            else:
+                org_words_w_aggregated_abb.append(w)
+        else:
+            org_words_w_aggregated_abb.append(w)
     
-    pattern = r'(?:\s[&])+'
-    result = re.findall(pattern, org_name)
-    if result:
-        for r in result:
-            org_name = org_name.replace(r, '&')
+    org_name = " ".join(org_words_w_aggregated_abb)
+    # pattern = r'(?:\.[a-z]\.)+'
+    # result = re.findall(pattern, org_name)
+    # if result:
+    #     for r in result:
+    #         pattern_w = r'(?:[a-z&])'
+    #         x = re.findall(pattern_w, r)
+    #         if r[-1] == ' ':
+    #             org_name = org_name.replace(r, x[0]+ ' ')
+    #         else:
+    #             org_name = org_name.replace(r, x[0])
+    
+    # pattern = r'(?:\s[&])+'
+    # result = re.findall(pattern, org_name)
+    # if result:
+    #     for r in result:
+    #         org_name = org_name.replace(r, '&')
     return org_name
 
 def remove_weird_characters(org_name: str):
@@ -28,5 +50,11 @@ def remove_weird_characters(org_name: str):
     return org_name
 
 if __name__ == "__main__":
-    s = 'F & J Davey Home'
-    print(remove_abbreviation(s))
+    s = 'P A T H Davey Home'
+    print(remove_weird_characters(remove_abbreviation(s)))
+
+    s = 'P.A.T.H Check check'
+    print(remove_weird_characters(remove_abbreviation(s)))
+
+    s =  'P. A. T. H. Check check'
+    print(remove_weird_characters(remove_abbreviation(s)))
